5f34675757ffa19b6a008ff87ddf1e086ed6ab06
Jason Nash <JasonRNash@gmail.com>
2025-12-31 21:12:17 -0600

    Refactor in prep for tactics (Fixes #58)

---
+++ fmassman.Client/Models/MatrixModels.cs
@@ -0,0 +1,10 @@
+namespace fmassman.Client.Models;
+
+public record MatrixColumn(string Key, string Name, string Category, string Phase = "");
+
+public class MatrixItem 
+{ 
+    public string Name { get; set; } = ""; 
+    public Dictionary<string, double> Scores { get; set; } = new(); 
+    public double GetScore(string key) => Scores.TryGetValue(key, out var val) ? val : 0; 
+}

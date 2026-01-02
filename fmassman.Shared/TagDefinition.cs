f2ce6cc962c78889ef72e9111f571ed2c5e618fb
Jason Nash <JasonRNash@gmail.com>
2026-01-01 22:13:21 -0600

    Fix for partition key case (Issue #59)

--- fmassman.Shared/TagDefinition.cs
+++ fmassman.Shared/TagDefinition.cs
@@ -1,9 +1,11 @@
 using System;
+using Newtonsoft.Json;
 
 namespace fmassman.Shared
 {
     public class TagDefinition
     {
+        [JsonProperty("id")]
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; } = string.Empty;
         public bool IsRostered { get; set; } = true;
